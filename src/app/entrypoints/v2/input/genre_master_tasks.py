df7fda51819b21b4bb794d7805d4fe3f56787340
yuya-okada527 <yuya.okada527@gmail.com>
2021-05-16 14:20:36 +0900

    add update genre tasks

--- src/app/entrypoints/v2/input/genre_master_tasks.py
+++ src/app/entrypoints/v2/input/genre_master_tasks.py
@@ -2,11 +2,28 @@ from typing import List
 
 from infra.client.tmdb.tmdb_api import init_tmdb_client
 from infra.repository.input.genre_repository import init_genre_repository
-from prefect import Flow, task
+from prefect import Flow, Parameter, task
+from service.input_service import exec_update_genre_master
 
 
 @task
-def update_genre_master():
+def update_genre_master(force_update: bool) -> None:
     print("update_genre_master")
+    # リポジトリの初期化
     genre_repository = init_genre_repository()
     tmdb_client = init_tmdb_client()
+
+    # サービスの実行
+    exec_update_genre_master(
+        force_update=force_update,
+        genre_repository=genre_repository,
+        tmdb_client=tmdb_client
+    )
+
+
+def update_genre_flow():
+    with Flow("Update Genre Master") as flow:
+        force_update = Parameter("force_update", default=True)
+        update_genre_master(force_update)
+
+    flow.run()
