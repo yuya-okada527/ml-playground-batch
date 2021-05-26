07dbbe8ed8985fb8d4fc7e0b4028baa00105bd9d
yuya-okada527 <yuya.okada527@gmail.com>
2021-05-26 22:40:49 +0900

    add movie detail task

--- src/app/entrypoints/v2/input/detail_movie_tasks.py
+++ src/app/entrypoints/v2/input/detail_movie_tasks.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from domain.models.internal.movie_model import MovieId
+from domain.models.internal.movie_model import Movie, MovieId
 from prefect import Flow, task
 
 
@@ -9,11 +9,23 @@ def truncate_movie_details() -> None:
     print("truncate_movie_details")
 
 @task
-def fetch_movie_ids() -> List[MovieId]:
+def extract_movie_ids() -> List[MovieId]:
     print("fetch_movie_ids")
     return []
 
+@task
+def extract_movie_detail(movie_id: MovieId) -> Movie:
+    print("fetch_movie_detail")
+    return None
+
+@task
+def load_movie_details(movies: List[Movie]) -> None:
+    print("load_movie_details")
+
 
 with Flow("Detail Movie Tasks") as flow:
-    truncate_movie_details()
-    fetch_movie_ids()
+    truncate_task = truncate_movie_details()
+    movie_ids = extract_movie_ids()
+    movies = extract_movie_detail.map(movie_id=movie_ids)
+    load_task = load_movie_details(movies)
+    load_task.set_upstream(truncate_task)
