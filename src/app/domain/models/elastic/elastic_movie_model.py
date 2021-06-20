a2a323cc3d444a57b646262801f772fe104d4d1a
yuya-okada527 <yuya.okada527@gmail.com>
2021-06-20 23:11:23 +0900

    add elastic search model

--- src/app/domain/models/elastic/elastic_movie_model.py
+++ src/app/domain/models/elastic/elastic_movie_model.py
@@ -1,2 +1,43 @@
-class ElasticMovieModel:
-    pass
+from elasticsearch_dsl import Document, Text, analyzer, connections, tokenizer
+from elasticsearch_dsl.field import Double, Integer, Keyword, Long
+
+trigram_analyzer = analyzer(
+    "trigram_analyzer",
+    type="custom",
+    tokenizer=tokenizer(
+        "trigram",
+        "nGram",
+        min_gram=2,
+        max_gram=3
+    ),
+    filter=[
+        "lowercase"
+    ]
+)
+
+
+class ElasticMovieModel(Document):
+    movie_id = Keyword(required=True)
+    free_word = Text(analyzer=trigram_analyzer)
+    original_title = Keyword()
+    japanese_title = Keyword()
+    overview = Keyword()
+    tagline = Keyword()
+    poster_path = Keyword()
+    backdrop_path = Keyword()
+    popularity = Double()
+    release_date = Keyword()
+    release_year = Integer()
+    genres = Integer(multi=True)
+    genre_labels = Keyword(multi=True)
+    keywords = Integer(multi=True)
+    keyword_labels = Keyword(multi=True)
+    index_time = Long()
+
+    class Index:
+        name = "movie"
+
+def init():
+    connections.create_connection(hosts=["localhost"])
+    if not connections.get_connection().indices.exists(index="movie"):
+        ElasticMovieModel.init()
---
+++ src/tests/integrations/test_elastic_movie_model.py
@@ -0,0 +1,29 @@
+import pytest
+from app.domain.models.elastic.elastic_movie_model import (ElasticMovieModel,
+                                                           init,
+                                                           trigram_analyzer)
+
+
+@pytest.mark.skip
+def test_save_and_get_same_movie():
+
+    init()
+
+    # テストデータ
+    test_id = 1
+    movie = ElasticMovieModel(meta={"id": test_id}, **{
+        "movie_id": "test"
+    })
+    movie.save()
+
+    assert movie.to_dict() == ElasticMovieModel.get(id=test_id).to_dict()
+
+
+def test_trigram_analyser_properly_tokenize():
+
+    response = trigram_analyzer.simulate("Hello")
+
+    actual = [t.token for t in response.tokens]
+    expected = ["he", "el", "ll", "lo", "hel", "ell", "llo"]
+
+    assert sorted(actual) == sorted(expected)
